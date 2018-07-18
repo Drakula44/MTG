37b8d8b6f39596672cdb63391e641202f58487e8
Drakula44 <ninadrakulic04@gmail.com>
2018-07-18 15:44:08 +0200

    Krecemo sa ispravljanjem bugova
    namesteni main menu  ali bez google play servica

--- M.T.G/Assets/Code/MainMenu.cs
+++ M.T.G/Assets/Code/MainMenu.cs
@@ -13,7 +13,7 @@ public class MainMenu : MonoBehaviour {
     {
         SceneManager.LoadScene("MainMode");
     }
-	public void Mode()
+	public void Point()
 	{
 		SceneManager.LoadScene("PointMode");
 	}
